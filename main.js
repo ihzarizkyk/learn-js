c098de87240fbb08c238c2c085a9adcb0d38c183
ihza rizky <ihzarizky30@gmail.com>
2021-03-20 16:17:27 +0700

    update

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
-alert("Hello World");
-var paraph = document.getElementById("p");
+var a,b;
+
+a = 10;
+b = 20;
+c = a+b;
+var e = document.getElementById("p").innerHTML("Hello World");
+console.log(e);
