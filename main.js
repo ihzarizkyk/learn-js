d1f10ebf68ed4a2fa27ac867a51d9f396b2abc4a
ihza rizky <ihzarizky30@gmail.com>
2021-04-27 21:01:26 +0700

    update getElementById

--- main.js
+++ main.js
@@ -3,5 +3,5 @@ var a,b,c;
 a = 10;
 b = 20;
 c = a+b;
-var e = document.getElementById("p").innerHTML("Hello World");
+var e = document.getElementById("p").innerHTML("Hello World") + c;
 console.log(e);
